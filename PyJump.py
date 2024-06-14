6d2909de36e2199d2f2b17d991615a6847379e45
siddhusathu20 <53175949+siddhusathu20@users.noreply.github.com>
2024-06-14 21:49:36 +0530

    Add files via upload

---
+++ PyJump.py
@@ -0,0 +1,250 @@
+# Class 12 project
+# by Siddharth Jai Gokulan
+
+import arcade
+import arcade.gui
+import sys
+import time
+import arcade.gui.widgets
+import arcade.gui.widgets.layout
+import arcade.gui.widgets.text
+
+X_MOVE_SPEED = 4
+Y_MOVE_SPEED = 10
+LEVEL_TIPS = {
+    1: """Press SPACE to jump.
+Your progress will be saved
+after clearing a level.""",
+    2: """NEW SKILL: Double Jump
+Press SPACE while in midair
+after jumping to jump again.""",
+    3: """NEW SKILL: Reverse Polarity
+Hold BACKSPACE to
+reverse your
+polarity.""",
+    4: """ZERO GRAVITY:
+Press SPACE to change
+your vertical movement
+direction."""
+}
+
+
+class Menu(arcade.View):
+    def __init__(self):
+        super().__init__()
+        self.manager = arcade.gui.UIManager()
+        self.title_text = arcade.Text("PyJump", self.window.width*0.5, self.window.height*0.7, arcade.csscolor.WHITE,
+                                      font_name="Bahnschrift" , font_size = 48, anchor_x="center", bold=True)
+        self.play_button = arcade.gui.UIFlatButton(text="PLAY", width=250)
+        self.button_anchor = self.manager.add(arcade.gui.UIAnchorLayout())
+        self.button_anchor.add(child=self.play_button)
+        @self.play_button.event("on_click")
+        def start_game(event):
+            game = Game()
+            game.setup()
+            self.window.show_view(game)
+    
+    def on_show_view(self):
+        self.window.background_color = arcade.csscolor.DARK_BLUE
+        self.manager.enable()
+        self.window.default_camera.use()
+    
+    def on_hide_view(self):
+        self.manager.disable()
+    
+    def on_draw(self):
+        self.clear()
+        self.manager.draw()
+        self.title_text.draw()
+    
+    def on_resize(self, width: int, height: int):
+        self.window.show_view(Menu())
+
+
+class Win(arcade.View):
+    def __init__(self):
+        super().__init__()
+        self.manager = arcade.gui.UIManager()
+        self.title_text = arcade.Text("Thanks for playing!", self.window.width*0.5, self.window.height*0.7, arcade.csscolor.WHITE,
+                                      font_name="Bahnschrift" , font_size = 48, anchor_x="center", bold=True)
+        self.play_button = arcade.gui.UIFlatButton(text="START OVER", width=250)
+        self.button_anchor = self.manager.add(arcade.gui.UIAnchorLayout())
+        self.button_anchor.add(child=self.play_button)
+        @self.play_button.event("on_click")
+        def start_game(event):
+            with open("save1.txt", "w+") as self.save1:
+                self.save1.write("1")
+            game = Game()
+            game.setup()
+            self.window.show_view(game)
+    
+    def on_show_view(self):
+        self.window.background_color = arcade.csscolor.GREEN
+        self.manager.enable()
+        self.window.default_camera.use()
+    
+    def on_hide_view(self):
+        self.manager.disable()
+    
+    def on_draw(self):
+        self.clear()
+        self.manager.draw()
+        self.title_text.draw()
+    
+    def on_resize(self, width: int, height: int):
+        self.window.show_view(Win())
+
+
+class Game(arcade.View):
+    def __init__(self):
+        super().__init__()
+        self.cube_texture = None
+        self.cube_sprite = None
+        self.camera = None
+        self.tiled_map = None
+        self.attempt = 1
+        self.attempt_text = None
+        self.anti_grav = False
+        self.zero_grav = False
+        self.grav_const = self.window.height/480
+        self.reverse = False
+        self.can_double_jump = False
+        self.can_reverse = False
+        try:
+            with open("save1.txt", "r+") as self.save1:
+                self.save1.seek(0)
+                self.level = int(self.save1.read())
+        except:
+            with open("save1.txt", "w+") as self.save1:
+                self.save1.write("1")
+            with open("save1.txt", "r+") as self.save1:
+                self.save1.seek(0)
+                self.level = int(self.save1.read())
+    
+    def setup(self):
+        self.window.background_color = arcade.csscolor.SKY_BLUE
+        self.cube_texture = arcade.load_texture("assets/sphere.png")
+        self.cube_sprite = arcade.Sprite(self.cube_texture, 0.03*(self.window.height/480))
+        self.cube_sprite.position = [50*(self.window.height/480), 70*(self.window.height/480)]
+        tile_layer_options = {
+            "Platforms": {"use_spatial_hash": True}
+        }
+        self.tiled_map = arcade.load_tilemap(f"assets/map{self.level}.json", scaling=self.window.height/480, layer_options=tile_layer_options)
+        self.scene = arcade.Scene.from_tilemap(self.tiled_map)
+        self.camera = arcade.camera.Camera2D()
+        self.attempt_text = arcade.Text(f"LEVEL {self.level}, ATTEMPT {self.attempt}", color=arcade.csscolor.BLACK,
+                                        x = 100*(self.window.height/480), y = 400*(self.window.height/480), font_size = 20, bold = True)
+        self.tip_text = arcade.Text(LEVEL_TIPS[self.level], multiline=True, width=400, color=arcade.csscolor.BLACK,
+                                    x = 100*(self.window.height/480), y = 350*(self.window.height/480), font_size = 20, bold = False)
+        self.grav_const = self.window.height/480
+        self.abin_sir = arcade.PhysicsEnginePlatformer(self.cube_sprite, walls=self.scene["Platforms"], gravity_constant=self.grav_const)
+        self.anti_grav = False
+        self.zero_grav = False
+        self.reverse = False
+        if self.level >= 2:
+            self.can_double_jump = True
+        if self.level >= 3:
+            self.can_reverse = True
+        if self.can_double_jump is True:
+            self.abin_sir.enable_multi_jump(2)
+        
+
+    def on_draw(self):
+        self.clear()
+        self.camera.use()
+        self.scene.draw()
+        self.cube_sprite.draw()
+        self.attempt_text.draw()
+        self.tip_text.draw()
+
+    def on_update(self, delta_time):
+        if self.reverse is True:
+            self.cube_sprite.change_x = -X_MOVE_SPEED*(self.window.height/480)
+        else:
+            self.cube_sprite.change_x = X_MOVE_SPEED*(self.window.height/480)
+        if (self.anti_grav is True and self.reverse is False) or (self.anti_grav is False and self.reverse is True):
+            self.cube_sprite.change_angle = -5
+        elif (self.anti_grav is False and self.reverse is False) or (self.anti_grav is True and self.reverse is True):
+            self.cube_sprite.change_angle = 5
+        self.camera.position = [self.cube_sprite.center_x+100, 240*(self.window.height/480)]
+        self.abin_sir.update()
+        anti_grav_block = arcade.check_for_collision_with_list(self.cube_sprite, self.scene["Anti Grav"])
+        for block in anti_grav_block:
+            block.remove_from_sprite_lists()
+            self.anti_grav = not self.anti_grav
+            self.grav_const = -self.grav_const
+            self.abin_sir = arcade.PhysicsEnginePlatformer(self.cube_sprite, walls = self.scene["Platforms"], gravity_constant = self.grav_const)
+            if self.can_double_jump is True:
+                self.abin_sir.enable_multi_jump(2)
+        zero_grav_block = arcade.check_for_collision_with_list(self.cube_sprite, self.scene["Zero Grav"])
+        for block in zero_grav_block:
+            block.remove_from_sprite_lists()
+            self.zero_grav = not self.zero_grav
+            if self.grav_const == 0:
+                if self.anti_grav is True:
+                    self.grav_const = -1*(self.window.height/480)
+                else:
+                    self.grav_const = self.window.height/480
+            else:
+                self.grav_const = 0
+                self.cube_sprite.change_y = (Y_MOVE_SPEED/1.5)*(self.window.height/480)
+            self.abin_sir = arcade.PhysicsEnginePlatformer(self.cube_sprite, walls = self.scene["Platforms"], gravity_constant = self.grav_const)
+            if self.can_double_jump is True:
+                self.abin_sir.enable_multi_jump(2)
+        if arcade.check_for_collision_with_list(self.cube_sprite, self.scene["Avoid"]):
+            self.attempt += 1
+            self.setup()
+        if arcade.check_for_collision_with_list(self.cube_sprite, self.scene["Finish"]):
+            if self.level < 4:
+                self.level += 1
+                with open("save1.txt", "r+") as self.save1:
+                    self.save1.seek(0)
+                    self.save1.write(f"{self.level}")
+                    self.save1.truncate()
+                    self.save1.seek(0)
+                self.attempt = 1
+                self.setup()
+            else:
+                self.window.show_view(Win())
+    
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.BACKSPACE and self.can_reverse is True:
+            self.reverse = True
+        if key == arcade.key.SPACE:
+            if self.zero_grav is False:
+                if self.anti_grav is False:
+                    if self.abin_sir.can_jump(5):
+                        self.abin_sir.increment_jump_counter()
+                        self.cube_sprite.change_y = Y_MOVE_SPEED*(self.window.height/480)
+                else:
+                    if self.abin_sir.can_jump(-5):
+                        self.abin_sir.increment_jump_counter()
+                        self.cube_sprite.change_y = -Y_MOVE_SPEED*(self.window.height/480)
+            else:
+                if self.cube_sprite.change_y != 0:
+                    self.cube_sprite.change_y = -self.cube_sprite.change_y
+                else:
+                    if self.abin_sir.can_jump(5):
+                        self.cube_sprite.change_y = Y_MOVE_SPEED/1.5
+                    elif self.abin_sir.can_jump(-5):
+                        self.cube_sprite.change_y = -Y_MOVE_SPEED/1.5
+        if key == arcade.key.R:
+            self.setup()
+        if key == arcade.key.ESCAPE:
+            self.window.show_view(Menu())
+        
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.SPACE and self.zero_grav is False:
+            self.cube_sprite.change_y = 0
+        if key == arcade.key.BACKSPACE and self.level >= 3:
+            self.reverse = False
+    
+    def on_resize(self, width: int, height: int):
+        self.window.show_view(Menu())
+    
+
+if __name__ == "__main__":
+    game_window = arcade.Window(640, 480, "PyJump", resizable=True)
+    menu = Menu()
+    game_window.show_view(menu)
+    arcade.run()
---
+++ assets/blocks.tsx
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="blocks" tilewidth="32" tileheight="32" tilecount="23" columns="0" tilerendersize="grid">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="8">
+  <image width="32" height="32" source="tiles/black_block (Tile).png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="tiles/blue_block (Tile).png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="tiles/green_diamond (Tile).png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="tiles/purple_block (Tile).png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="tiles/purple_spike_down (Tile).png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="tiles/purple_spike_up (Tile).png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="tiles/rainbow_diamond (Tile).png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="tiles/red_block (Tile).png"/>
+ </tile>
+ <tile id="18">
+  <image width="32" height="32" source="tiles/purple_side_block (Tile).png"/>
+ </tile>
+ <tile id="19">
+  <image width="32" height="32" source="tiles/red_side_block (Tile).png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="32" source="tiles/blue_diamond (Tile).png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="tiles/orange_block_new (Tile).png"/>
+ </tile>
+ <tile id="22">
+  <image width="32" height="32" source="tiles/blue_block_new (Tile).png"/>
+ </tile>
+ <tile id="23">
+  <image width="32" height="32" source="tiles/orange_up (Tile).png"/>
+ </tile>
+ <tile id="24">
+  <image width="32" height="32" source="tiles/orange_left (Tile).png"/>
+ </tile>
+ <tile id="25">
+  <image width="32" height="32" source="tiles/orange_right (Tile).png"/>
+ </tile>
+ <tile id="26">
+  <image width="32" height="32" source="tiles/orange_down (Tile).png"/>
+ </tile>
+ <tile id="28">
+  <image width="32" height="32" source="tiles/orange_spike_up (Tile).png"/>
+ </tile>
+ <tile id="29">
+  <image width="32" height="32" source="tiles/orange_spike_down (Tile).png"/>
+ </tile>
+ <tile id="30">
+  <image width="32" height="32" source="tiles/blue_block_angle (Tile).png"/>
+ </tile>
+ <tile id="31">
+  <image width="32" height="32" source="tiles/orange_block (Tile).png"/>
+ </tile>
+ <tile id="32">
+  <image width="32" height="32" source="tiles/blue_block_exp2 (Tile).png"/>
+ </tile>
+ <tile id="33">
+  <image width="32" height="32" source="tiles/blue_block_exp (Tile).png"/>
+ </tile>
+</tileset>
